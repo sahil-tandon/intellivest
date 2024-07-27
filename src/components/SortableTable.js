9b89d24945e4d7f07972303bb384c590eb18c022
Sahil Tandon <sahil.tandon@live.com>
2024-07-28 01:45:10 +0530

    IntelliVest | Ensures sorting a column on one table doesn't affect other tables on the page.

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaSort, FaSortUp, FaSortDown } from "react-icons/fa";
 
-function SortableTable({ columns, data, sortConfig, onSort, actions }) {
+function SortableTable({ columns, data, actions }) {
+  const [sortConfig, setSortConfig] = useState({
+    key: null,
+    direction: "ascending",
+  });
+
+  const sortedData = React.useMemo(() => {
+    if (!sortConfig.key) return data;
+    return [...data].sort((a, b) => {
+      if (a[sortConfig.key] < b[sortConfig.key]) {
+        return sortConfig.direction === "ascending" ? -1 : 1;
+      }
+      if (a[sortConfig.key] > b[sortConfig.key]) {
+        return sortConfig.direction === "ascending" ? 1 : -1;
+      }
+      return 0;
+    });
+  }, [data, sortConfig]);
+
+  const requestSort = (key) => {
+    setSortConfig((prevConfig) => {
+      if (prevConfig.key === key) {
+        return {
+          ...prevConfig,
+          direction:
+            prevConfig.direction === "ascending" ? "descending" : "ascending",
+        };
+      }
+      return { key, direction: "ascending" };
+    });
+  };
+
   const getSortIcon = (columnKey) => {
     if (sortConfig.key === columnKey) {
       return sortConfig.direction === "ascending" ? (
@@ -22,7 +53,7 @@ function SortableTable({ columns, data, sortConfig, onSort, actions }) {
               <th
                 key={column.key}
                 className="px-6 py-3 cursor-pointer"
-                onClick={() => onSort(column.key)}
+                onClick={() => requestSort(column.key)}
               >
                 {column.label} {getSortIcon(column.key)}
               </th>
@@ -31,7 +62,7 @@ function SortableTable({ columns, data, sortConfig, onSort, actions }) {
           </tr>
         </thead>
         <tbody>
-          {data.map((item) => (
+          {sortedData.map((item) => (
             <tr key={item.id} className="bg-background border-b border-border">
               {columns.map((column) => (
                 <td
